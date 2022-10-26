08083bde11f70417254fdbf3b9409ab176f3a471
Pedro La Rosa D <pedrolarosad@gmail.com>
2022-10-26 23:25:55 +0200

    fix(linked-list): define public package\'s api

--- packages/linked-list/src/index.ts
+++ packages/linked-list/src/index.ts
@@ -1,5 +1,10 @@
+import { LinkedList } from './linked-list';
+
 export { RawLinkedList } from './core/raw-linked-list';
 export { LinkedListNode } from './core/linked-list-node';
 
 export type { DoublyLinkedListNode } from './core/linked-list-node';
 export type { LinkedListDataStructure } from './core/raw-linked-list';
+export type { DoublyLinkedList } from './linked-list';
+
+export default LinkedList;
---
+++ packages/linked-list/src/linked-list/index.ts
@@ -0,0 +1,2 @@
+export { LinkedList } from './linked-list';
+export type { DoublyLinkedList } from './types';
